151b3818b1158e73b6e23812022800477ae34e0c
Peder Wiig Aalen <peder.wiig@gmail.com>
2022-09-01 19:54:16 +0200

    Update dns-updater.sh

--- dns-updater.sh
+++ dns-updater.sh
@@ -3,19 +3,18 @@ FILE=$LOGFILE
 WORKING_DIR=$(pwd)
 ENV_FILE=$WORKING_DIR/.env.json
 
-if ! command -v jq &> /dev/null; then
-    msg="jq is not installed, please install jq"
-    echo $msg >> $FILE
-    exit 1
-fi
-
-
 if [ -z "$FILE" ]; then # check if there is an environment - if not export default one
     export LOGFILE=$WORKING_DIR/log.txt
     if [ ! -f "$LOGFILE" ]; then # if logfile does not exit create one.
         touch $LOGFILE
     fi
     FILE=$LOGFILE
+fi
+
+if ! command -v jq &> /dev/null; then
+    msg="jq is not installed, please install jq"
+    echo $msg >> $FILE
+    exit 1
 fi
 
 if [ ! -f "$ENV_FILE" ]; then
